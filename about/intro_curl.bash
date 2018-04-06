f08588691b282a3d9ec551ec759a6b3264e2a29b
Michael Mayer <mjmayer@ucdavis.edu>
2018-04-06 16:29:50 -0700

    Add code example using curl to check email

---
+++ about/intro_curl.bash
@@ -0,0 +1,51 @@
+$curl -s -X GET https://mailsac.com/api/addresses/admin%40mailsac.com/messages | jq ".[0]"
+
+{
+  "_id": "BotvTxaona7gLID1Adtpfj8Fnfi7HSSv-0",
+  "from": [
+    {
+      "address": "microsoftstore@e.microsoft.com",
+      "name": "Microsoft Store"
+    }
+  ],
+  "to": [
+    {
+      "address": "admin@mailsac.com",
+      "name": ""
+    }
+  ],
+  "cc": null,
+  "bcc": null,
+  "subject": "Ahoy, Sea of Thieves for PC is here",
+  "savedBy": null,
+  "originalInbox": "admin@mailsac.com",
+  "inbox": "admin@mailsac.com",
+  "domain": "mailsac.com",
+  "received": "2018-03-29T18:28:07.732Z",
+  "size": 23420,
+  "attachments": null,
+  "ip": "65.55.234.211",
+  "via": "144.202.71.79",
+  "folder": "inbox",
+  "labels": [],
+  "read": null,
+  "rtls": true,
+  "links": [
+    "href=https://e.microsoft.com/Key-3567701.C.CQZpy.C.K0.-.CMHlNS",
+    "http://msstorepromoemail.blob.core.windows.net/windows-store-edits/Nav_MSFT_Logo.jpg",
+    "http://msstorepromoemail.blob.core.windows.net/windows-store-edits/Nav_WhiteSpace330.jpg",
+    "href=https://e.microsoft.com/Key-3567701.C.CQZpy.D.K0.-.CRfmrT",
+    "https://msstorepromoemail.blob.core.windows.net/15198-ws-final-fantasy-sea-of-thieves-ga-en-us/15198_WS_Sea_of_Thieves_GA_HCL_01.jpg",
+    "https://msstorepromoemail.blob.core.windows.net/15198-ws-final-fantasy-sea-of-thieves-ga-en-us/15198_WS_Sea_of_Thieves_GA_HCL_02.jpg",
+    "href=https://e.microsoft.com/Key-3567701.C.CQZpy.F.K0.-.C1qsMW",
+    "https://msstorepromoemail.blob.core.windows.net/15198-ws-final-fantasy-sea-of-thieves-ga-en-us/15198_WS_Sea_of_Thieves_GA_HCL_03.jpg",
+    "href=https://e.microsoft.com/Key-3567701.C.CQZpy.G.K0.-.C60tqX",
+    "https://support.xbox.com/games/game-setup/cross-play.",
+    "href=https://e.microsoft.com/Key-3567701.C.CQZpy.H.K0.-.CcBw4Y",
+    "https://support.xbox.com/games/game-titles/xbox-play-anywhere-help.",
+    "href=https://e.microsoft.com/Key-3567701.C.CQZpy.J.K0.-.CpMBp0",
+    "href=https://account.microsoft.com/profile/unsubscribe?CTID=0&ECID=jIce0uXtDC5qRlyCYqZsz5yCLndUMgkkElK5ftB2%2B2E%3D&K=5fdd51f6-8b3e-4bec-9878-4edd2310dfda&CMID=419741&D=636579325644736709&PID=18000&TID=00000000-0000-0000-0000-000000000001",
+    "href=https://e.microsoft.com/Key-3567701.C.CQZpy.K.K0.-.CtkD31"
+  ],
+  "spam": 1.3370381090039505e-09
+}
--- about/introduction.rst
+++ about/introduction.rst
@@ -2,3 +2,15 @@
 
 Introduction
 ============
+Until now, interacting with email was limitted using desktop or web based 
+clients. Mailsac allows you to interact with your email via REST API,
+webhooks and websockets. How easy is it? easy. 
+
+--------------------------------------------
+
+You can use curl to view emails sent to admin@mailsac.com
+
+.. literalinclude:: intro_curl.bash
+    :language: bash
+    :emphasize-lines: 1
+    :lines: 1-35,49-51
