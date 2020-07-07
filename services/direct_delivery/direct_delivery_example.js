b3601d827be3199ecf24393f86f52eb02fca2ff1
Michael Mayer <mjmayer@ucdavis.edu>
2020-07-07 06:48:46 -0700

    Refactor direct delivery js example to use promises

--- services/direct_delivery/direct_delivery_example.js
+++ services/direct_delivery/direct_delivery_example.js
@@ -1,18 +1,35 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer')
 
-async function main() {
-  const transporter = nodemailer.createTransport({
-    host: 'in.mailsac.com"',
-    port: 587,
-    secure: false
-  });
+const transporter = nodemailer.createTransport({
+  host: 'in.mailsac.com',
+  port: 587,
+  // will use TLS by upgrading later in the connection with STARTTLS
+  secure: false
+})
 
-  const info = await transporter.sendMail({
-    from: '"Mywebapp Customer Support" no-reply@mywebapp.com',
-    to: 'mycustomer@gmail.com',
-    subject: 'Password reset',
-    text: 'Click on the link to reset you password\r\n' +
-      'https://mywebapp.com/password-reset/PasswordResetToken'
-  });
+transporter.sendMail({
+  from: '"Mywebapp Customer Support" no-reply@mywebapp.com',
+  to: 'mycustomer@gmail.com',
+  subject: 'Password reset',
+  text: `Click on the link to reset you password\r\n
+    https://mywebapp.com/password-reset/PasswordResetToken`
+})
+  .then(response => {
+    console.log('Successfully sent mail', response)
+  })
+  .catch(err => {
+    console.log('Failed sending mail', err)
+  })
+
+/*
+Successfully sent mail {
+  accepted: [ 'mycustomer@gmail.com' ],
+  rejected: [],
+  envelopeTime: 129,
+  messageTime: 136,
+  messageSize: 413,
+  response: '250 OK : queued as 71jiTaUqliiQRb5xMHxja0gU',
+  envelope: { from: 'no-reply@mywebapp.com', to: [ 'mycustomer@gmail.com' ] },
+  messageId: '<15574d0b-2a51-d854-5755-e2e1b0d29d4f@mywebapp.com>'
 }
-main().catch(console.error);
+*/
