247d48202da1dab252447620b47855c5046a94a9
Michael Mayer <mjmayer@ucdavis.edu>
2020-07-07 06:52:55 -0700

    Add console debug output to python direct delivery example

--- services/direct_delivery/direct_delivery_example.py
+++ services/direct_delivery/direct_delivery_example.py
@@ -29,3 +29,37 @@ except Exception as e:
     print('Error: ', e)
 else:
     print('Email Sent')
+
+"""
+send: 'ehlo [127.0.1.1]\r\n'
+reply: b'250-smtp-in2-172-31-42-57 Welcome! [172.31.42.57:25]\r\n'
+reply: b'250-SIZE 1048576\r\n'
+reply: b'250-STARTTLS\r\n'
+reply: b'250-PROXY PROXY\r\n'
+reply: b'250 HELP\r\n'
+reply: retcode (250); Msg: b'smtp-in2-172-31-42-57 Welcome! [172.31.42.57:25]\nSIZE 1048576\nSTARTTLS\nPROXY PROXY\nHELP'
+send: 'STARTTLS\r\n'
+reply: b'220 Ready to start TLS\r\n'
+reply: retcode (220); Msg: b'Ready to start TLS'
+send: 'ehlo [127.0.1.1]\r\n'
+reply: b'250-smtp-in2-172-31-42-57 Welcome! [172.31.42.57:25]\r\n'
+reply: b'250-SIZE 1048576\r\n'
+reply: b'250-PROXY PROXY\r\n'
+reply: b'250 HELP\r\n'
+reply: retcode (250); Msg: b'smtp-in2-172-31-42-57 Welcome! [172.31.42.57:25]\nSIZE 1048576\nPROXY PROXY\nHELP'
+send: 'mail FROM:<no-reply@mywebapp.com> size=303\r\n'
+reply: b'250 Accepted\r\n'
+reply: retcode (250); Msg: b'Accepted'
+send: 'rcpt TO:<mycustomer@gmail.com>\r\n'
+reply: b'250 Accepted\r\n'
+reply: retcode (250); Msg: b'Accepted'
+send: 'data\r\n'
+reply: b'354 Enter message, ending with "." on a line by itself\r\n'
+reply: retcode (354); Msg: b'Enter message, ending with "." on a line by itself'
+data: (354, b'Enter message, ending with "." on a line by itself')
+send: b'Content-Type: text/plain; charset="us-ascii"\r\nMIME-Version: 1.0\r\nContent-Transfer-Encoding: 7bit\r\nSubject: Password reset\r\nFrom: Mywebapp Customer Support <no-reply@mywebapp.com>\r\nTo: mycustomer@gmail.com\r\n\r\nClick on the link to reset you password\r\nhttps://mywebapp.com/password-reset/PasswordResetToken\r\n.\r\n'
+reply: b'250 OK : queued as qpihn6fZNaxKsk_WrCEZjl_JNeuUo\r\n'
+reply: retcode (250); Msg: b'OK : queued as qpihn6fZNaxKsk_WrCEZjl_JNeuUo'
+data: (250, b'OK : queued as qpihn6fZNaxKsk_WrCEZjl_JNeuUo')
+Email Sent
+"""
