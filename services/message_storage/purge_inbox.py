6e4245a749a2675eee9a0167b30fa791f10441ed
Jeff P <jeffhparrish@gmail.com>
2020-08-20 08:37:30 -0700

    Update purge_inbox.py

--- services/message_storage/purge_inbox.py
+++ services/message_storage/purge_inbox.py
@@ -1,7 +1,7 @@
 import requests
 
 headers = {'Mailsac-Key': 'YOUR_API_KEY_HERE'}
-url = 'https://mailsac.com/api/addresses/bclinton@mailsac.com/messages/'
+url = 'https://mailsac.com/api/addresses/bclinton@mailsac.com/messages'
 r = requests.delete(url, headers=headers)
 print(r.status_code)
 
